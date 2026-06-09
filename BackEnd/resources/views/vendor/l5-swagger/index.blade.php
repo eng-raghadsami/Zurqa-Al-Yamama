f63193c2c7c31638f24689dd65d6f7407d20fc00
Raghad Sami <raghadsami.w@gmail.com>
2026-06-09 14:39:02 +0300

    Fix Swagger docs URL

--- BackEnd/resources/views/vendor/l5-swagger/index.blade.php
+++ BackEnd/resources/views/vendor/l5-swagger/index.blade.php
@@ -126,7 +126,7 @@
         const urls = [];
 
         @foreach($urlsToDocs as $title => $url)
-            urls.push({name: "{{ $title }}", url: "{{ $url }}"});
+            urls.push({name: "{{ $title }}", url: "{{ \Illuminate\Support\Str::before($url, '?') }}"});
         @endforeach
 
         // Build a system
